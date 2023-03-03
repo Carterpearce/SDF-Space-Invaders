dacd2094e8981afd538251575e02b0d7dd97dc57
Carter <0706539@schoolsnet.act.edu.au>
2023-03-03 12:32:42 +1100

    options
    wrr

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,10 +8,17 @@ func _ready():
 	set_physics_process(true)
 
 func _process(delta):
-	if Input.is_action_just_pressed("fire"):
-		var bulletInstance = bulletSource.instance()
-		bulletInstance.position = Vector2(position.x, position.y-20)
-		get_tree().get_root().add_child(bulletInstance)
+	if GlobalVariables.automaticFiring:
+		if GlobalVariables.bulletInstanceCount < 3:
+			if Input.is_action_just_pressed("fire"):
+				var bulletInstance = bulletSource.instance()
+				bulletInstance.position = Vector2(position.x, position.y-20)
+				get_tree().get_root().add_child(bulletInstance)
+	elif Input.is_action_just_pressed("fire"):
+		if GlobalVariables.bulletInstanceCount < 3:
+			var bulletInstance = bulletSource.instance()
+			bulletInstance.position = Vector2(position.x, position.y-20)
+			get_tree().get_root().add_child(bulletInstance)
 		
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
