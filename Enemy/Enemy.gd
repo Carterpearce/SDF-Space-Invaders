89512449a43e7a4e48cfa33a85a0d555989efb78
Carter <0706539@schoolsnet.act.edu.au>
2023-05-15 14:32:08 +1000

    enemy firing
    enemys attack now

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,14 +1,33 @@
 extends KinematicBody2D
 
+var bullet = preload("res://Bullet-Enemy/Bullet-Enemy.tscn")
+
+
+	
 func _ready():
-	print("enemy ready")
 	$Area2D.connect("area_entered", self, "_colliding")
 	
-	
+
 func _colliding(area):
-	if area.is_in_group("left"):
-		get_parent().global_position.y += 10
-		get_parent().speed = get_parent().speed * -1
 	if area.is_in_group("right"):
+		#print("emenies collide right")
 		get_parent().global_position.y += 10
-		get_parent().speed = get_parent().speed * -1
+		get_parent().speed = -100
+	if area.is_in_group("left"):
+		#print("emenies collide left")
+		get_parent().global_position.y += 10
+		get_parent().speed = 100
+
+
+func _process(delta):
+#	while (true):
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var my_random_number = rng.randf_range(2.0, 30.0)
+	#print("time: ",my_random_number)
+	yield(get_tree().create_timer(my_random_number), "timeout")
+	if GlobalVariables.enemyBulletInstanceCount < 5:
+		var bulletInstance = bullet.instance()
+		
+		bulletInstance.position = Vector2(global_position.x, global_position.y+20)
+		get_tree().get_root().add_child(bulletInstance)
